08b3ae5b11b77ab00ec07487ebb9b53c01b1f4f2
Wangbin <bin.wang@thoughtworks.com>
2020-03-01 20:38:41 +0800

    [wangbin] delete died code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,8 +3,6 @@ package com.gildedrose;
 import java.util.Arrays;
 
 class GildedRose {
-    private final int MAX_QUALITY = 50;
-    private final int MIN_QUALITY = 0;
     Item[] items;
 
     public GildedRose(Item[] items) {
